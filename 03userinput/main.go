e51460cac1d3a631bd276d9e3c634783a4cc4d85
bhanuprakash321 <bhanup@vigorushealth.com>
2023-07-07 10:07:20 +0530

    2nd

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome to User Input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza:")
+
+	// comma ok syntax|| err ok
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for Rating", input)
+	fmt.Printf("type of the rating is %T", input)
+
+}
