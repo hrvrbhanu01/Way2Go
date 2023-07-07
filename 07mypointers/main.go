3af8c56c1438920012b1342259aab4d83f852d30
bhanuprakash321 <bhanup@vigorushealth.com>
2023-07-07 23:39:30 +0530

    Co-authored-by: Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>

---
+++ 07mypointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to a class on pointers")
+
+	//var ptr *int
+
+	//fmt.Println("Value of pointer is ", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+
+	fmt.Println("Value of actual pointer is ", ptr)
+	fmt.Println("Value of actual pointer is ", *ptr)
+
+}
