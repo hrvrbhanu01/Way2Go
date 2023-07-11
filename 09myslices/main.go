51af0333909fb2eea9614b88ff0b6614cc82049b
bhanuprakash321 <bhanup@vigorushealth.com>
2023-07-11 10:43:16 +0530

    slices

---
+++ 09myslices/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to Slices")
+
+	var fruitList = []string{"Apple", "Leachy", "Tomato"}
+	fmt.Println("Type of fruitList is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3]) //first entity will not be available and from position 1 to 3, last range is non-inclusive
+	fmt.Println(fruitList)
+
+	highScores := make([]int, 4) //make(type of data, number of data values)
+
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+
+	highScores = append(highScores, 555, 666, 321)
+
+	fmt.Println(highScores)
+
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+
+	fmt.Println(sort.IntsAreSorted(highScores))
+
+}
