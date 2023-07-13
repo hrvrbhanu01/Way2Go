9ef9353aa9cc9a05f617f9ef6114e690c4e585c6
bhanuprakash321 <bhanup@vigorushealth.com>
2023-07-13 10:14:46 +0530

    methods

---
+++ 16methods/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Structs in Golang")
+
+	//no inheritence in go; No super or parent or no child i.e.classes are not there in go.
+
+	hitesh := User{"Bhanu", "hitesh@go.dev", true, 18}
+	fmt.Println(hitesh)
+	fmt.Printf("hitesh details are: %+v\n", hitesh)
+	fmt.Printf("Name is %v and email is %v\n", hitesh.Name, hitesh.Email)
+	hitesh.GetStatus()
+	hitesh.NewMail()
+
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (u User) GetStatus() {
+	fmt.Println("Is user active: ", u.Status)
+
+}
+
+func (u User) NewMail() {
+	u.Email = "test@go.dev"
+	fmt.Println("Email of the user is : ", u.Email)
+}
