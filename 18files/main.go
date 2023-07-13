b17117268b163d21bcd07d95cdf0cc1f8d94e98d
bhanuprakash321 <bhanup@vigorushealth.com>
2023-07-13 11:15:23 +0530

    files

---
+++ 18files/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files in Go")
+	content := "This needs to go in a file - LearnCodeOnline"
+
+	file, err := os.Create("./mylcogofile.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+	fmt.Println("length is : ", length)
+	defer file.Close()
+	readFile("./mylcogofile.txt")
+}
+
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+
+	fmt.Println("Text data inside the file is \n", string(databyte))
+
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
