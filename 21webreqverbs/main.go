ce3f557c483fd1bbdc9cc308ea14ce0e20057558
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-10-30 15:10:48 +0530

    1

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to we request verbs!")
+	fmt.Println("Welcome to web request video!")
+	PerformGetRequest()
 }
 
 func PerformGetRequest() {
-	const myurl = ""
+	const myurl = "http://localhost:8000/get"
 
 	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("Bytecount is: ", byteCount)
+	fmt.Println(responseString.String())
+	// fmt.Println(content)
+	// fmt.Println(string(content))
 
 }
