7c70aa407b3c38b11368267438f2a141863d8bd4
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-11-01 12:40:47 +0530

    01/11(3)

--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // model for course - file
@@ -31,7 +33,7 @@ func main() {
 
 }
 
-//controllers - file
+//controllers - file(i.e it will have different files)
 
 // serve home route
 func serveHome(w http.ResponseWriter, r *http.Request) {
@@ -43,4 +45,22 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
 
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get One Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r) //get the parameters
+
+	// loop through the courses , find the matching id and return the response
+	for _, course:=range courses{
+		if course.CourseId==params["id"]{
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course found with Given ID")
+	return
 }
