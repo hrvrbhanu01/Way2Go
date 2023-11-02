41b47ad1701bf9cb6a4ffcd7ca48775535cd4d4e
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-11-02 18:24:37 +0530

    02/11(4)

--- 25mongoAPI/model/models.go
+++ 25mongoAPI/model/models.go
@@ -1,12 +1,11 @@
 package model
 
-import(
+import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Netflix struct{
-	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Movie string `json:"movie,omitempty"`
-	Watched bool `json:"watched,omitempty"`
+type Netflix struct {
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Movie   string             `json:"movie,omitempty"`
+	Watched bool               `json:"watched,omitempty"`
 }
-
